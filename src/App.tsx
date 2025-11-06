720f345a3f2a23179ab88cfc42019c18c4185f41
AshwinJ127 <108308607+AshwinJ127@users.noreply.github.com>
2025-11-06 01:56:39 -0800

    Fixed for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import type { ElementType, Dispatch, SetStateAction } from 'react';
-import { Home, User, Code, Briefcase, Rocket, Mail, Menu, X, Github, Linkedin, Copy, ExternalLink } from 'lucide-react';
+import { Home, User, Code, Briefcase, Rocket, Mail, Menu, X } from 'lucide-react';
 
 import './App.css';
 import HomePage from './Pages/HomePage';
